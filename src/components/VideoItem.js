7a61fdd0fdaa3b50bd35f0358650a2699a68966e
Roberto-Livi <robertomlivi@outlook.com>
2020-09-22 01:23:12 -0400

    rendering video thumbnails

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -3,7 +3,7 @@ import React from 'react'
 const VideoItem = ({ video }) => {
     return (
         <div>
-            <img src={video.snippet.thumbnails.medium.url} />
+            <img src={video.snippet.thumbnails.medium.url} alt="video" />
             <div>{video.snippet.title}</div>
         </div>
     )
